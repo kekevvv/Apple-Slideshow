4c8eeb36a2ad3a5b8622a69f1ab0d922b530e92c
kekevvv <706413005@qq.com>
2018-11-13 23:16:28 +0800

    Apple Slideshow

---
+++ js/main.js
@@ -0,0 +1,50 @@
+var buttons = document.querySelectorAll('.menu > ul > .button');
+var images = document.querySelector('.images');
+var gallery = document.querySelector('.gallery');
+
+for(let i = 0;i<buttons.length;i++){
+    buttons[i].addEventListener('click',function(event){
+        event.preventDefault();
+        var index = getIndex(event.currentTarget);
+        images.style.cssText = `transform:translateX(${-index*920}px);`;
+        event.currentTarget.classList.add('active');
+        removeAcitve(event.currentTarget);
+    });
+}
+
+var n = 0;
+var size = buttons.length;
+buttons[n%size].click();
+var timer = createTimer();
+gallery.addEventListener('mouseenter',()=>{
+    window.clearInterval(timer);
+})
+gallery.addEventListener('mouseleave',()=>{
+    timer = createTimer();
+})
+
+function createTimer(){
+    return setInterval(()=>{
+            n+=1;
+            buttons[n%size].click();
+        },2000);
+}
+
+function getIndex(element){
+    var allChildren = element.parentNode.children;
+    var n = 0;
+    for(let i = 0;i<allChildren.length;i++){
+        if(allChildren[i] === element){
+            n = i;
+        }
+    }
+    return n;
+}
+function removeAcitve(element){
+    var allChildren = element.parentNode.children;
+    for(let i = 0;i<allChildren.length;i++){
+        if(allChildren[i] !== element){
+            allChildren[i].classList.remove('active');
+        }
+    }
+}
